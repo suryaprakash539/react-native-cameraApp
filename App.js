f332525fcffc21216783b0469649f60f79a60b54
suryaprakash <surya.zvky@gmail.com>
2020-11-27 20:18:26 +0530

    made use of RNCamera and captured image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Image, View, Text} from 'react-native';
+import {StyleSheet, Image, View, Text, TouchableOpacity} from 'react-native';
 
 import {RNCamera} from 'react-native-camera';
 
@@ -21,14 +21,78 @@ const App = () => {
     try {
       const options = {quality: 0.9, base64: false};
       const data = await camera.takePictureAsync(options);
-      console.log(data);
+      // console.log(data);
       setImage(data.uri);
     } catch (error) {
       console.warn(error);
     }
   };
 
-  return <></>;
+  return (
+    <>
+      <View style={styles.container}>
+        {image ? (
+          <Text>Image is Present</Text>
+        ) : (
+          <RNCamera
+            style={styles.preview}
+            type={RNCamera.Constants.Type.front}
+            captureAudio={false}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'To click a picture',
+              buttonPositive: 'OK',
+              buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+              title: 'Permission to use audio',
+              message: 'To record audio',
+              buttonPositive: 'OK',
+              buttonNegative: 'Cancel',
+            }}>
+            {({camera, status}) => {
+              if (status !== 'READY') return <PendingView />;
+              return (
+                <View
+                  style={{
+                    flex: 0,
+                    flexDirection: 'row',
+                  }}>
+                  <TouchableOpacity
+                    style={styles.capture}
+                    onPress={() => takePicture(camera)}>
+                    <Text>Snap</Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            }}
+          </RNCamera>
+        )}
+      </View>
+    </>
+  );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#0A79DF',
+  },
+
+  preview: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+
+  capture: {
+    flex: 0,
+    backgroundColor: 'orange',
+    padding: 10,
+    alignSelf: 'center',
+  },
+});
